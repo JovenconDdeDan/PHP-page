acc5316ed15a33316f0ca37a364cf85ee2f6894e
JovenDan <danilojcantorm@gmail.com>
2025-04-09 21:37:51 -0500

    rfrf

--- loginsystem.sql
+++ loginsystem.sql
@@ -1,22 +1,20 @@
+CREATE DATABASE `loginid`;
 
-CREATE DATABASE  'loginusb';
-
-CREATE TABLE 'loginid' (
-    'userid' int(11) NOT NULL,
-    'username' varchar(25) NOT NULL,
-    'email' varchar(30) NOT NULL,
-    'password' varchar(20) NOT NULL,
+CREATE TABLE `loginid` (
+    `userid` int(11) NOT NULL,
+    `username` varchar(25) NOT NULL,
+    `email` varchar(30) NOT NULL,
+    `password` varchar(20) NOT NULL
 );
 
-INSERT INTO 'loginid' ('userid', 'username', 'email', 'password') VALUES
+INSERT INTO `loginid` (`userid`, `username`, `email`, `password`) VALUES
 (1,'tatiana743', 'ttamk1@gmail.com', 'sandwichseco8'),
 (2,'docjohntor', 'medicom34@gmail.com', '6ehf83nsj'),
 (3,'reconocedorpro', 'cramirez@gmail.com', 'motitas67'),
-(4,'cramirez', 'cramirez@gmail.com', 'motitas67');
+(4,'cramirez', 'cramirez@gmail.com','motitas67');
 
-ALTER TABLE 'loginid' 
-    ADD PRIMARY KEY ('userid');
+ALTER TABLE `loginid` 
+    ADD PRIMARY KEY (`userid`);
 
-ALTER TABLE 'loginid'
-    MODIFY 'userid' int(11) NOT NULL AUTO_INCREMENT;
-COMIT;
+ALTER TABLE `loginid`
+    MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT;
