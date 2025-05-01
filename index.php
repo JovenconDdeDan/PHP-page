a55a0653fb6989ce3a2c8939165b9d4a10f39ef2
JovenDan <danilojcantorm@gmail.com>
2025-04-30 21:28:37 -0500

    subir

---
+++ index.php
@@ -0,0 +1,24 @@
+<html> 
+
+    <head>
+    <title> Login / Signup PHP </title>
+
+        <!-- se referencian archivos externos que se utilizan en la página -->
+         <link ref= "stylesheet" href="style.css">
+
+    </head>
+
+    <!-- en el body es donde se especifica la estructura de la página -->
+
+    <body>
+
+        <h1> Bienvenido, por favor regístrate o accede a la página </h1>
+
+        <div id="enlaces">
+
+            <a href = "login.php"> si tiene cuenta, acceda aquí</a>
+
+            <a href = "signup.php"> Regístrese aquí</a>
+
+    </body>
+</html>
---
+++ signup.php
@@ -0,0 +1,13 @@
+<html>
+
+    <head>
+        <title> Sign up </title>
+        <link ref = "stylesheet" href = "style.css">
+    </head>
+
+    <body>
+
+
+
+    </body>
+</html>
