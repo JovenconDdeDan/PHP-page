c00f4a40b69db7d240dc9e13f2d3b07efc92cb1d
JovenDan <61570423+JovenconDdeDan@users.noreply.github.com>
2025-05-08 00:14:41 +0000

    archives

---
+++ includes/connection.php
@@ -0,0 +1,14 @@
+<?php
+
+// se crean las variables para la conexión a la base de datos
+
+$server = "localhost";
+$username = "root";
+$password = "motitas2020";
+$database = "login_registro";
+
+// se cera la conexión a la base de datos
+
+$conn = mysqli_connect($server, $username, $password, $database);
+
+?>
